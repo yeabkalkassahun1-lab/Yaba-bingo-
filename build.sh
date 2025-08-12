85b08d5f7464974904f4285739db5f8296e7ea1f
yeabkalkassahun1-lab <yeabkalkassahun1@gmail.com>
2025-08-12 14:34:08 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# build.sh - FINAL
-set -o errexit
+# A simple build script for Render
 
-pip install --upgrade pip
+set -o errexit  # Exit on error
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
